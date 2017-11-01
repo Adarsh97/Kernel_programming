4145eddbc290b6a5d55f4bd62e33eceb19cfa44f
Adarsh Sunilkumar <adarshsunilkumar55@gmail.com>
2017-11-01 16:34:33 +0530

    added hello.c

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+
+static int hello_init(void)
+{
+printk(KERN_ALERT"TEST:Hello world \n");
+
+return 0;
+}
+
+static void hello_exit(void)
+{
+printk(KERN_ALERT"TEST:hello world is exiting.");
+return 0;
+
+
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
