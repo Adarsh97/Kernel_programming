adfd1e5fab32cabcdaa6c2aacaf996895d189dbd
Adarsh Sunilkumar <adarshsunilkumar55@gmail.com>
2017-11-01 17:04:21 +0530

    added param_var

--- hello.c
+++ hello.c
@@ -2,18 +2,44 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
 
+///step1
+
+#include<linux/moduleparam.h>
+
+
+
+//step2
+
+int param_var =0;
+
+//step 3 register (macro)
+// module_param(name_var,type,permissions)
+
+module_param(param_var,int,S_IRUSR | S_IWUSR);
+
+void displays(void)
+{
+printk(KERN_ALERT"TEST:param =%d",param_var);
+
+}
 
 static int hello_init(void)
 {
+// purpose is to register functionalities & allocate resources
 printk(KERN_ALERT"TEST:Hello world \n");
-
+displays();
 return 0;
 }
 
 static void hello_exit(void)
 {
+// purpose is to unregister functionalities & deallocate registers
+
 printk(KERN_ALERT"TEST:hello world is exiting.");
-return 0;
+
+
+
+
 
 
 }
